7ed351163d1b923df9157a1add03eeafb62b5469
Vincent <vincopi@googlemail.com>
2023-07-03 19:38:26 +0200

    I guess i remove some comment

--- bump-bot/bump_bot.py
+++ bump-bot/bump_bot.py
@@ -35,7 +35,6 @@ async def reaction_changed(payload: discord.RawReactionActionEvent, added: bool)
 	message = await message_cache.get_message(payload.message_id, channel)	
 	if message:
 
-		# reaction_removed events do not have a member included while reaction_added ones do.
 		member = channel.guild.get_member(payload.user_id)
 		if added:
 			found_reactions_cache.add_found_reaction(payload.message_id, payload.emoji, member)
