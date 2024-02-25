62d0a018ace78c945c4a35f55654792945d8fcb5
Julian Berger <24358794+Junber@users.noreply.github.com>
2024-02-25 18:00:51 +0100

    Automatically announce results of date poll

--- bump-bot/commands/bump_command.py
+++ bump-bot/commands/bump_command.py
@@ -1,15 +1,22 @@
-from typing import override
+import asyncio
+from typing import Tuple, override
 import discord
 
+from cancelable_wait import CancelableWait
 import bump_bot_config as config
 from commands.voting_command import VotingCommand
+from commands.bump_time_command import BumpTimeCommand
 import discord_client
 import found_reactions_cache
 
 
 class BumpCommand(VotingCommand):
+    announce_results_wait = CancelableWait(300.0)
+
     @staticmethod
-    def get_embed(found_reactions: found_reactions_cache.FoundReactions) -> discord.Embed:
+    def get_embed_and_result(
+        found_reactions: found_reactions_cache.FoundReactions,
+    ) -> Tuple[discord.Embed, list[str] | None]:
         members_who_voted = set()
         for reaction in found_reactions:
             members_who_voted |= found_reactions[reaction]
@@ -34,25 +41,34 @@ class BumpCommand(VotingCommand):
 
         message_text += "\n"
 
+        possible_days: list[str] = []
         for reaction in config.get_bump_reactions():
             members = set()
             if reaction in found_reactions:
                 members = found_reactions[reaction]
 
+            day_name = config.get_bump_reactions()[reaction]
+
             if len(members) == len(members_who_voted):
                 message_text += ":white_check_mark:"
+                possible_days.append(day_name)
             else:
                 message_text += ":x:"
 
-            message_text += " {} ({}): ".format(
-                config.get_bump_reactions()[reaction], discord_client.get_emoji(reaction)
-            )
+            message_text += " {} ({}): ".format(day_name, discord_client.get_emoji(reaction))
 
             message_text += VotingCommand.member_list(members, members_who_voted, "No one")
             message_text += "\n"
 
-        return discord.Embed(title=config.get_bump_embed_title()).add_field(
-            name=config.get_bump_embed_field_name(), value=message_text
+        result = None
+        if len(members_who_voted) >= config.get_required_votes():
+            result = possible_days
+
+        return (
+            discord.Embed(title=config.get_bump_embed_title()).add_field(
+                name=config.get_bump_embed_field_name(), value=message_text
+            ),
+            result,
         )
 
     @override
@@ -66,8 +82,9 @@ class BumpCommand(VotingCommand):
     async def send_poll(
         self, message: discord.Message, channel: discord.TextChannel
     ) -> discord.Message:
+        (embed, _) = self.get_embed_and_result({})
         poll_message: discord.Message = await channel.send(
-            config.get_bump_message_content(), embed=self.get_embed({})
+            config.get_bump_message_content(), embed=embed
         )
         await found_reactions_cache.initialize_empty_reactions(poll_message.id)
         for reaction in config.get_bump_reactions():
@@ -89,11 +106,33 @@ class BumpCommand(VotingCommand):
             and message.author == discord_client.get_client().user
         )
 
+    async def announce_results(self, result: list[str], channel: discord.TextChannel) -> None:
+        if not await self.announce_results_wait.wait():
+            return
+
+        if len(result) == 0:
+            await channel.send(
+                "It seems like no date could be found, sorry. Better luck next time."
+            )
+        elif len(result) == 1:
+            await channel.send("Only one date works, so we'll use that.")
+            bump_time_message = await channel.send(config.get_voting_trigger() + " " + result[0])
+
+            # TODO: avoid creating new instance here
+            await BumpTimeCommand().on_message(bump_time_message, channel)
+        else:
+            await channel.send("Multiple days work. Sort that one out yourselves.")
+
     @override
     async def on_reaction_changed(self, message: discord.Message, reaction_added: bool) -> None:
         if not await self.update_message_wait.wait():
             return
 
-        await message.edit(
-            embed=self.get_embed(await found_reactions_cache.get_found_reactions(message))
+        (embed, result) = self.get_embed_and_result(
+            await found_reactions_cache.get_found_reactions(message)
         )
+
+        await message.edit(embed=embed)
+
+        if result is not None and isinstance(message.channel, discord.TextChannel):
+            asyncio.create_task(self.announce_results(result, message.channel))
